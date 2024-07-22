d43a86cfc087a4776b4a65256a7372b710a485b6
revathiVinoth <revzz.1993@gmail.com>
2024-07-22 08:11:10 +0530

    resassign

--- app/colorPairUtils.js
+++ app/colorPairUtils.js
@@ -20,26 +20,25 @@ function getColorFromPairNumber(pairNumber) {
 
 function getPairNumberFromColor(pair) {
   let majorIndex = -1;
-  for (let i = 0; i < MajorColorNames.length; i++) {
-    if (MajorColorNames[i] == pair.majorColor) {
-      majorIndex = i;
-      break;
-    }
-  }
+  reassignMajorOrMinorIndex(pair, majorIndex);
 
   let minorIndex = -1;
-  for (let i = 0; i < MinorColorNames.length; i++) {
-    if (MinorColorNames[i] == pair.minorColor) {
-      minorIndex = i;
-      break;
-    }
-  }
+  reassignMajorOrMinorIndex(pair, minorIndex);
 
   if (majorIndex == -1 || minorIndex == -1) {
     throw `Unknown Colors:${pair.toString()}`;
   }
 
   return (majorIndex * MinorColorNames.length) + (minorIndex + 1);
+}
+
+function reassignMajorOrMinorIndex(pair, index) {
+  for (let i = 0; i < MajorColorNames.length; i++) {
+    if (MajorColorNames[i] == pair.majorColor) {
+      index = i;
+      break;
+    }
+  }
 }
 
 export default { getColorFromPairNumber, getPairNumberFromColor }
